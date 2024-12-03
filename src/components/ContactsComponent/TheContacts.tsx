1c3686e57453cafe37a1ad37d8269c1ecd3906b5
islomalarov <islomalarov@gmail.com>
2024-12-03 11:51:46 +0500

    add translate contactsComp

--- src/components/ContactsComponent/TheContacts.tsx
+++ src/components/ContactsComponent/TheContacts.tsx
@@ -1,25 +1,27 @@
 import "@/scss/globals.scss";
 import styles from "./page.module.scss";
+import { useTranslations } from "next-intl";
 
 export const TheContacts = () => {
+  const t = useTranslations("ContactsPage");
   return (
     <div>
-      <h1 className="title">Kontaktlar</h1>
+      <h1 className="title">{t("title")}</h1>
       <div className={styles.info}>
         <div className={styles.block}>
-          <h2>Telefon raqami</h2>
+          <h2>{t("phone")}</h2>
           <p>+998 90 830-20-00</p>
         </div>
         <div className={styles.block}>
-          <h2>Manzil</h2>
+          <h2>{t("address")}</h2>
           <p>Toshkent shahri, Yashnobod tumani, Istiqbol ko'chasi, 21-uy</p>
         </div>
         <div className={styles.block}>
-          <h2>Mo'ljal</h2>
+          <h2>{t("landmark")}</h2>
           <p>Westminster chorraxasi</p>
         </div>
         <div className={styles.block}>
-          <h2>Email</h2>
+          <h2>{t("email")}</h2>
           <p>info@yashil-energiya.uz</p>
         </div>
       </div>
