f1130197150c34b3796522d47837331e2f723e8d
islomalarov <islomalarov@gmail.com>
2025-03-24 12:40:19 +0500

    feat: about page has been updated

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import "@/scss/globals.scss";
 import s from "./page.module.scss";
 import { TheHero } from "@/src/components/HeroComponent/TheHero";
@@ -10,6 +12,7 @@ import solarMan from "@/public/partners/solarMan.png";
 import foxEss from "@/public/partners/foxEss.png";
 import tosh from "@/public/partners/tosh.jpg";
 import tw from "@/public/partners/tw.jpg";
+import { motion } from "motion/react";
 
 export default function About() {
   const t = useTranslations("AboutPage");
@@ -31,66 +34,69 @@ export default function About() {
       />
       <div className="container">
         <div className={s.content}>
-          <div className={s.info}>
-            <div className={s.block}>
-              <p>{t("paragraph1")}</p>
-            </div>
-            <div className={s.block}>
-              <h2 className={s.title}>{t("title1")}:</h2>
-              <p>{t("paragraph2")}</p>
-            </div>
-            <div className={s.projectsBlock}>
-              <h2 className={s.title}>{t("title2")}</h2>
-              <ul className={s.projectsList}>
-                {projects.map((project) => {
-                  const projectSrc = t(`${project}.src`);
-                  const projectImageSrc = require(
-                    `@/public/aboutPage/${projectSrc}`,
-                  ).default;
-
-                  return (
-                    <li key={project} className={s.projectsItem}>
-                      <div>
-                        <Image
-                          className={s.projectAva}
-                          src={projectImageSrc}
-                          alt={projectSrc}
-                        />
-                      </div>
-                      <p>{t(`${project}.title`)}</p>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-            <div className={s.block}>
-              <h2 className={s.title}>{t("title3")}</h2>
-              <div className={s.achievements}>
-                {achievements.map((achievement) => (
-                  <div key={achievement} className={s.achievement}>
-                    <Icon
-                      icon={t(`${achievement}.src`)}
-                      color="#12903e"
-                      fontSize={100}
-                    />
-                    <p className={s.achievementDescription}>
-                      {t(`${achievement}.description`)}
-                    </p>
-                  </div>
-                ))}
-              </div>
-            </div>
-            {/* <div className={s.block}>
-              <h3 className={s.title}>{t("title4")}</h3>
-              <div className={s.partners}>
-                <Image src={tw} alt="tw" />
-                <Image src={foxEss} alt="foxEss" />
-                <Image src={huawei} alt="huawei" />
-                <Image src={tosh} alt="tosh" />
-                <Image src={solarMan} alt="solarMan" />
-              </div>
-            </div> */}
+          <div className={s.titleBlock}>
+            <p>{t("paragraph1")}</p>
           </div>
+          <div className={s.descriptionBlock}>
+            <h2 className={s.title}>{t("title1")}:</h2>
+            <p>{t("paragraph2")}</p>
+          </div>
+          <div className={s.projectsBlock}>
+            <h2 className={s.title}>{t("title2")}</h2>
+            <ul className={s.projectsList}>
+              {projects.map((project) => {
+                const projectSrc = t(`${project}.src`);
+                const projectImageSrc =
+                  require(`@/public/aboutPage/${projectSrc}`).default;
+
+                return (
+                  <li key={project} className={s.projectsItem}>
+                    <div>
+                      <Image
+                        className={s.projectAva}
+                        src={projectImageSrc}
+                        alt={projectSrc}
+                      />
+                    </div>
+                    <p>{t(`${project}.title`)}</p>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <div className={s.achievementsBlock}>
+            <h2 className={s.title}>{t("title3")}</h2>
+            <div className={s.achievements}>
+              {achievements.map((achievement) => (
+                <div key={achievement} className={s.achievement}>
+                  <Icon
+                    icon={t(`${achievement}.src`)}
+                    color="#12903e"
+                    fontSize={100}
+                  />
+                  <p className={s.achievementDescription}>
+                    {t(`${achievement}.description`)}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+          <motion.div
+            className={s.partnersBlock}
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, ease: "easeOut" }}
+            viewport={{ once: true, amount: 0.2 }}
+          >
+            <h3 className={s.title}>{t("title4")}</h3>
+            <div className={s.partners}>
+              <Image src={tw} alt="tw" />
+              <Image src={foxEss} alt="foxEss" />
+              <Image src={huawei} alt="huawei" />
+              <Image src={tosh} alt="tosh" />
+              <Image src={solarMan} alt="solarMan" />
+            </div>
+          </motion.div>
         </div>
       </div>
       <TheFeedback />
