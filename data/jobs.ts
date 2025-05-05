cd0eb667bf6842c6d90046cfdff7dfb81d134259
islomalarov <islomalarov@gmail.com>
2025-05-05 12:37:08 +0500

    add new vacancy

--- data/jobs.ts
+++ data/jobs.ts
@@ -13,4 +13,12 @@ export const jobs = [
       "Министерство энергетики Республики Узбекистан получило Специальный фонд подготовки проекта (PPSF), далее именуемый «Грант», в размере эквивалента 1 700 000 долларов США от Азиатского банка инфраструктурных инвестиций (AIIB) на подготовительные мероприятия по проекту «Развитие государственной распределенной солнечной энергетики в Узбекистане» (UPDSED) и намеревается к применять часть из  выручка за консультационные услуги.",
     url: "url2",
   },
+  {
+    id: 3,
+    title:
+      "National Environmental and Social Specialist (Individual Consultant)",
+    description:
+      "The Ministry of Energy of the Republic of Uzbekistan has received Project Preparation Special Fund (PPSF), hereafter referred to as “the Grant”, in the amount of US$ 1,700,000 equivalent from the Asian Infrastructure Investment Bank (AIIB) towards the project preparatory activities for of the Uzbekistan Public Distributed Solar Energy Development (UPDSED) Project and intends to apply part of the proceeds for consulting services.",
+    url: "url2",
+  },
 ];
