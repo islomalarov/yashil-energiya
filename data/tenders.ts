c3f8cc99cab591cb9923b8acc4bf9cfa4fdd733d
islomalarov <islomalarov@gmail.com>
2025-05-23 18:25:30 +0500

    feat: add TendersPage

---
+++ data/tenders.ts
@@ -0,0 +1,12 @@
+export const tenders = [
+  {
+    id: 1,
+    projectNumber: "P000827",
+    projectNameRu: "Проект развития",
+    projectNameEn: "Project development",
+    projectDescriptionRu:
+      "Республика Узбекистан получила Специальный фонд подготовки проекта (PPSF), далее именуемый «Грант», в размере, эквивалентном 1 700 000 долл. США, от Азиатского банка инфраструктурных инвестиций (далее – АБИИ) на подготовительные мероприятия по проекту развития государственной распределенной солнечной энергетики в Узбекистане и намерена использовать часть средств на оплату товаров, работ, неконсультационных услуг и консультационных услуг, которые будут закуплены в рамках этого гранта.",
+    projectDescriptionEn:
+      "The Republic of Uzbekistan has received a Project Preparation Special Fund (PPSF) grant in the amount equivalent to US$1,700,000 from the Asian Infrastructure Investment Bank (AIIB) for preparatory activities for the project to develop state distributed solar energy in Uzbekistan and intends to use part of the funds to pay for goods, works, non-consulting services and consulting services to be procured under this grant.",
+  },
+];
