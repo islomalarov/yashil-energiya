33c1f9e1ae1eccf6951a376b7d8a7e24ed1ecabc
Amir Ibragimov <amir.ibragimov1991@gmail.com>
2025-06-03 16:51:16 +0500

    feat: number update plus email hidden

--- data/branches.ts
+++ data/branches.ts
@@ -82,15 +82,15 @@ export const branches = [
   {
     region: "region12",
     name: "Zakirov Bahodir",
-    phone: "+998 90 830-31-08",
-    address: "Toshkent shahri, Istiqbol ko‘chasi, 21",
+    phone: "+998 95 324-22-52",
+    address: "Toshkent shahri, Yunusobod tumani, Chingiz Aytmatov ko'chasi, 2B-uy",
     mail: "toshkentvil.servis@yashil-energiya.uz",
   },
   {
     region: "region13",
-    name: "Zakirov Bahodir",
-    phone: "+998 90 830-31-08",
-    address: "Toshkent shahri, Istiqbol ko‘chasi, 21",
+    name: "Muhamadjonov Fozilxon",
+    phone: "+998 95 324-22-72",
+    address: "Toshkent shahri, Yunusobod tumani, Chingiz Aytmatov ko'chasi, 2B-uy",
     mail: "info@yashil-energiya.uz",
   },
   {
