361c4fb074b3e3502801bfa24a38ed2bdd6a22f5
Amir Ibragimov <a.ibragimov@yashil-energiya.uz>
2026-05-15 11:50:06 +0500

    added security policy

--- next.config.js
+++ next.config.js
@@ -1,26 +1,72 @@
 const createNextIntlPlugin = require("next-intl/plugin");
 const withNextIntl = createNextIntlPlugin();
 
+const securityHeaders = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "strict-origin-when-cross-origin",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=()",
+  },
+  {
+    key: "Content-Security-Policy",
+    value: `
+      default-src 'self';
+      script-src 'self' 'unsafe-inline' 'unsafe-eval' https://challenges.cloudflare.com;
+      style-src 'self' 'unsafe-inline';
+      img-src 'self' data: blob: https://us-west-2.graphassets.com;
+      font-src 'self' data:;
+      connect-src 'self' https://challenges.cloudflare.com https://*.upstash.io;
+      frame-src https://challenges.cloudflare.com;
+      worker-src 'self' blob:;
+      object-src 'none';
+      base-uri 'self';
+      form-action 'self';
+      frame-ancestors 'self';
+      upgrade-insecure-requests;
+    `
+      .replace(/\s{2,}/g, " ")
+      .trim(),
+  },
+];
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export",
-  // // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
-  // // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
-  // // Optional: Change the output directory `out` -> `dist`
-  // distDir: "dist",
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "us-west-2.graphassets.com",
         port: "",
-        // pathname: "/account123/**",
       },
     ],
-    // loader: "custom",
-    //   loaderFile: "./my/image/loader.ts",
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
   },
 };
 
