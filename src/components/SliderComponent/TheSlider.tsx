47bf09e1cdbe072bec7531229afa8144b981b257
Amir Ibragimov <a.ibragimov@yashil-energiya.uz>
2026-05-21 18:20:53 +0500

    M2 Add homepage hero CTA

--- src/components/SliderComponent/TheSlider.tsx
+++ src/components/SliderComponent/TheSlider.tsx
@@ -2,6 +2,7 @@
 import styles from "./page.module.scss";
 import TheVideoPlayer from "../VideoPlayerComponent/TheVideoPlayer";
 import { getTranslations } from "next-intl/server";
+import { Link } from "@/i18n/navigation";
 
 export const TheSlider = async () => {
   const t = await getTranslations("HomePage");
@@ -20,6 +21,15 @@ export const TheSlider = async () => {
       <div className="container">
         <div className={styles.info}>
           <h1 className={styles.title}>{t("heroTitle")}</h1>
+          <p className={styles.descr}>{t("heroSubtitle")}</p>
+          <div className={styles.actions}>
+            <Link className={styles.primaryLink} href="/contacts">
+              {t("ctaContact")}
+            </Link>
+            <Link className={styles.secondaryLink} href="/plants">
+              {t("ctaProjects")}
+            </Link>
+          </div>
         </div>
       </div>
     </div>
